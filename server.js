430a8385e08ef0f03d35b5d9cf046132bd8b1051
igoratf <igor.atf@gmail.com>
2019-01-27 00:22:01 -0300

    change register post

--- server.js
+++ server.js
@@ -1,11 +1,93 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(cors());
+
+const tempDatabase = {
+   users: [
+      {
+         id: '123',
+         name: 'Donkey',
+         email: 'donkey@email.com',
+         password: 'kong',
+         entries: 0,
+         joined: new Date()
+      }
+   ],
+
+   login: [
+      {
+         id: '987',
+         hash: '',
+         email: '',
+      }
+   ]
+
+}
+
 
 app.get('/', (req, res) => {
-   res.send('this is working');
-})
+   res.send(tempDatabase.users);
+});
+
+app.post('/signin', (req, res) => {
+   if (req.body) {
+      tempDatabase.users.forEach(user => {
+         if (user.email === req.body.email && user.password === req.body.password) {
+            res.json('success');
+         }
+      })
+   }
+   res.status(400).json('error logging in');
+});
+
+app.post('/register', (req, res) => {
+   const { email, password, name } = req.body;
+   console.log('req', req.body);
+   if (req.body) {
+      tempDatabase.users.push({
+         id: 125,
+         email: email,
+         password: password,
+         name: name,
+         entries: 0,
+         joined: new Date()
+         
+      });
+      res.json(tempDatabase.users[tempDatabase.users.length - 1]);
+   }
+
+   res.status(400).json('error registering');
+
+});
+
+app.get('/profile/:id', (req, res) => {
+   const { id } = req.params;
+   tempDatabase.users.forEach(user => {
+      if (user.id === id) {
+         return res.json(user);
+      }
+   })
+
+   return res.status(404).json('user not found');
+});
+
+
+app.put('/image', (req, res) => {
+   const { id } = req.body;
+   tempDatabase.users.forEach(user => {
+      if (user.id === id) {
+         return res.json(++user.entries);
+      }
+   })
+
+   return res.status(404).json('user not found');
+});
 
 app.listen(3000, () => {
    console.log('listening port 3000');
