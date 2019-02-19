1c0d722060769c170835148395138e645c819fa5
igoratf <igor.atf@gmail.com>
2019-02-18 22:18:14 -0300

    return error on register

--- controllers/register.js
+++ controllers/register.js
@@ -26,7 +26,7 @@ const handleRegister = (database, bcrypt) => (req, res) => {
          .then(trx.commit)
          .catch(trx.rollback);
    })
-      .catch((err) => console.log(err));
+      .catch((err) => res.status(400).json('not able to register'));
 
 
 };
